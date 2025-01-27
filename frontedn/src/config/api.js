5169ce28b9e592ea7c98cf7e8a2794b6dd188c37
Md Sohil <127312964+Sohil75@users.noreply.github.com>
2025-01-27 18:45:16 +0530

    Update api.js
    url

--- frontedn/src/config/api.js
+++ frontedn/src/config/api.js
@@ -1,9 +1,9 @@
 
 import axios from 'axios';
-const DEPLOYED='https://urbanbackend-ubk8.onrender.com'
+const DEPLOYED='https://ecommercebackedn.onrender.com'
 const LOCALHOST='http://localhost:5454'
 
-export const API_BASE_URL = LOCALHOST;
+export const API_BASE_URL = DEPLOYED;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
